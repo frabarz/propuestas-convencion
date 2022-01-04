f65a6be02833d4ecca1f0af631eebe4cb73f8714
Francisco Abarzua <git.id@frabarz.cl>
2022-01-04 15:51:38 -0300

    adds async semaphore to throttle requests

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,5 @@
 import { cheerio, TagElement } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+import { Semaphore } from "https://deno.land/x/semaphore@v1.1.0/mod.ts";
 
 const filename = Deno.args[0];
 const buffer = await Deno.readFile(filename);
@@ -8,11 +9,13 @@ const html = decoder.decode(buffer);
 const $ = cheerio.load(html);
 const cards = $(".card.iniciativa");
 
+const semaphore = new Semaphore(10);
+
 // Create and store the category dictionary
 const categories: Record<string, string> = {};
 
 // Get all the proposals in the index page and save them at `index.json`
-const proposals = Array.from(cards, (card) => {
+const proposals: Proposal[] = Array.from(cards, (card) => {
   const $ = cheerio.load(card);
   const ini = card as TagElement;
 
@@ -39,24 +42,45 @@ await Deno.writeTextFile(
 // Fetch the detailed page contents and save them in the `propuestas/` folder
 await Deno.mkdir("propuestas", { recursive: true });
 await Promise.all(
-  proposals.map(async (proposal) => {
-    const response = await fetch(proposal.href);
-    const buffer = await response.arrayBuffer();
-    const html = decoder.decode(buffer);
+  proposals.map((proposal) =>
+    fetchProposal(proposal)
+      .then((html) => {
+        const detailed = { ...proposal, ...parseProposal(html) };
+        const filename = "./propuestas/" + proposal.id + ".json";
+        return Deno.writeTextFile(filename, JSON.stringify(detailed, null, 2));
+      }, (error) => {
+        console.error(`Error fetching proposal ${proposal.id}:`, error.message);
+      })
+  ),
+);
 
-    const $ = cheerio.load(html, { decodeEntities: false });
-    const prop = $("#propuesta").html() as string;
+async function fetchProposal(proposal: Proposal): Promise<string> {
+  const release = await semaphore.acquire();
+  const response = await fetch(proposal.href);
+  const buffer = await response.arrayBuffer();
+  release();
+  return decoder.decode(buffer);
+}
 
-    const topic = $(".objeto.detalle p").text().replace("Tema:", "").trim();
+function parseProposal(html: string): Pick<Proposal, "topic" | "outline"> {
+  const $ = cheerio.load(html, { decodeEntities: false });
+  const prop = $("#propuesta").html() as string;
 
-    const outline = prop.split("<h2>").slice(1).map((item) => {
+  return {
+    topic: $(".objeto.detalle p").text().replace("Tema:", "").trim(),
+    outline: prop.split("<h2>").slice(1).map((item) => {
       const [title, ...contents] = item.split("</h2>").map((key) => key.trim());
       return [title, contents.join("").replace(/<\/?(?:p|br)>/g, "")];
-    });
+    }),
+  };
+}
 
-    await Deno.writeTextFile(
-      "./propuestas/" + proposal.id + ".json",
-      JSON.stringify({ ...proposal, topic, outline }, null, 2),
-    );
-  }),
-);
+interface Proposal {
+  title: string;
+  href: string;
+  supporters: number;
+  id: number;
+  category: string;
+  topic?: string;
+  outline?: string[][];
+}
